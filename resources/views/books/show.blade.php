57b1a53b002ee3404ef9addd79e3621aed9eeb98
Revontulian <pablo.canteria@gmail.com>
2025-06-19 12:38:17 +0200

    Adding individual book view

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <h2>Book title - </h2>
+
+    <div class="bg-gray-200 p-4 rounded">
+        <p><strong>Author:</strong> </p>
+        <p><strong>Published:</strong> </p>
+        <p><strong>ISBN:</strong> </p>
+        <p><strong>Genre:</strong> </p>
+        <p><strong>Public:</strong> </p>
+    </div>
+
+</x-layout>

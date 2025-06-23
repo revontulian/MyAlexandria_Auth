f3ae56261ab23fa2892e2989f9650c7103be3c62
Revontulian <pablo.canteria@gmail.com>
2025-06-23 16:46:41 +0200

    Adding show data

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
-    <h2>Book title - </h2>
+    <h2>Title: {{ $book->title }}</h2>
 
-    <div class="bg-gray-200 p-4 rounded">
-        <p><strong>Author:</strong> </p>
-        <p><strong>Published:</strong> </p>
-        <p><strong>ISBN:</strong> </p>
-        <p><strong>Genre:</strong> </p>
-        <p><strong>Public:</strong> </p>
+    <div class="bg-amber-200 p-4 rounded border border-amber-400">
+        <p><strong>Author:</strong> {{ $book->author }}</p>
+        <p><strong>Published:</strong> {{ $book->published_date }}</p>
+        <p><strong>ISBN:</strong> {{ $book->isbn }}</p>
+        <p><strong>Genre:</strong> {{ $book->genre }}</p>
+        <p><strong>Public:</strong> {{ $book->is_public ? 'Yes' : 'No' }}</p>
     </div>
 
 </x-layout>

3d2db22cd7a709e0a4c31e658de8d3a63b150cef
Revontulian <pablo.canteria@gmail.com>
2025-06-23 16:34:34 +0200

    Improving add form usability

--- resources/views/books/add.blade.php
+++ resources/views/books/add.blade.php
@@ -6,26 +6,26 @@
 
         <!-- Book title -->
         <label for="title">Title:</label>
-        <input type="text" id="title" name="title" value="" required>
+        <input type="text" id="title" name="title" value="{{ old('title') }}" required>
 
         <!-- Book author -->
         <label for="author">Author:</label>
-        <input type="text" id="author" name="author" value="" required>
+        <input type="text" id="author" name="author" value="{{ old('author') }}" required>
 
         <!-- Book ISBN -->
         <label for="isbn">ISBN:</label>
-        <input type="number" id="isbn" name="isbn" value="" required>
+        <input type="number" id="isbn" name="isbn" value="{{ old('isbn') }}" required>
 
         <!-- Book publication date -->
         <label for="published_date">Published Date:</label>
-        <input type="date" id="published_date" name="published_date" value="">
+        <input type="date" id="published_date" name="published_date" value="{{ old('published_date') }}" required>
 
         <!-- Book genre -->
         <label for="genre">Genre:</label>
         <select id="genre" name="genre" required>
             <option value="" disable selected>Select a genre</option>
             @foreach ($genres as $genre)
-            <option value="{{ $genre }}">{{ $genre }}</option>
+            <option value="{{ $genre }}" {{ old('genre') == $genre ? 'selected' : '' }}>{{ $genre }}</option>
             @endforeach
         </select>
 
@@ -33,7 +33,7 @@
         <!-- Book public status -->
         <label for="is_public" class="block mb-2">Do you want this book to be public?</label>
         <input type="hidden" name="is_public" value="0">
-        <input type="checkbox" id="is_public" name="is_public" value="1" checked>
+        <input type="checkbox" id="is_public" name="is_public" value="1" {{ old('is_public') ? 'checked' : '' }}>
 
         <!-- Submit button -->
         <button type="submit" class="btn mt-4">Add Book</button>

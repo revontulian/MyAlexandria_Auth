f8f544df20a327ab55c2598a8cf3d4d2484f54f2
Revontulian <pablo.canteria@gmail.com>
2025-06-24 11:49:06 +0200

    Middle step register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,3 @@
 <x-layout>
-    <x-slot name="title">Register</x-slot>
-
-    <form method="POST" action="{{ route('show.register') }}">
-        @csrf
-        <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" required>
-        </div>
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit">Register</button>
-    </form>
+    <h2>Register</h2>
 </x-layout>

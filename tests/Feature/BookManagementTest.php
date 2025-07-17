a69846875dbbd0a08bbe2858943005e4455c92fb
Revontulian <pablo.canteria@gmail.com>
2025-07-17 12:22:08 +0200

    Adding tests for book endpoints

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -6,20 +6,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Book;
 
 class BookManagementTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-    /**
-     * A basic feature test example.
-     */
-    
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 
     public function test_book_creation()
     {
@@ -52,13 +43,57 @@ class BookManagementTest extends TestCase
         $response->assertUnauthorized();
     }
 
-    public function test_book_retrieval()
+    public function test_books_retrieval()
     {
         $user = User::factory()->create();
         $this->actingAs($user, 'api');
 
         $response = $this->getJson('/api/books');
         $response->assertOk();
-       
+    }
+
+    public function test_single_book_retrieval()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api');
+        $book = Book::factory()->create([
+            'user_id' => $user->id,
+        ]);
+        $response = $this->getJson('/api/books/' . $book->id);
+        $response->assertOk();
+    }
+
+    public function test_book_deletion()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api');
+
+        $book = Book::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->deleteJson('/api/books/' . $book->id);
+        $response->assertStatus(201);
+    }
+
+    public function test_book_update()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api');
+
+        $book = Book::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->putJson('/api/books/' . $book->id, [
+            'title' => 'Updated Book Title',
+            'author' => 'Updated Author',
+            'isbn' => '1234567890123',
+            'genre' => 'Updated Genre',
+            'published_date' => now(),
+            'is_public' => false,
+        ]);
+
+        $response->assertOk();
     }
 }

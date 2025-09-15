c7e3cd22709c75f0b2e6a28840eb9d9242044277
Revontulian <pablo.canteria@gmail.com>
2025-09-15 12:59:30 +0200

    Add sidebar for better usability

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,35 +25,47 @@
             @endguest
 
             @auth
-                <span class="border-r-2 pr-2">
-                    @auth
-                    <span class="username">Hello, {{ auth()->user()->name }}</span>
-                    @endauth
-                </span>
+            <span class="border-r-2 pr-2">
+                @auth
+                <span class="username">Hello, {{ auth()->user()->name }}</span>
+                @endauth
+            </span>
 
-                @php
-                    $rolesArray = json_decode(auth()->user()->roles, true);
-                @endphp
+            @php
+            $rolesArray = json_decode(auth()->user()->roles, true);
+            @endphp
 
-                @if (in_array('admin', $rolesArray))
-                    <a href="{{ route('users.admin') }}" class="btn">Admin Panel</a>
-                @endif
+            @if (in_array('admin', $rolesArray))
+            <a href="{{ route('users.admin') }}" class="btn">Admin Panel</a>
+            @endif
 
-                <a href="{{ route('books.add') }}" class="btn">Add a book</a>
-
-                <form method="POST" action="{{ route('logout') }}" class="m-0">
-                    @csrf
-                    <button type="submit" class="btn">Logout</button>
-
-                </form>
+            
+            <form method="POST" action="{{ route('logout') }}" class="m-0">
+                @csrf
+                <button type="submit" class="btn">Logout</button>
+                
+            </form>
             @endauth
-
+            
         </nav>
     </header>
+    
+    <div class="flex">
+        <aside class="w-48 bg-gray-100 min-h-screen p-4">
+            @auth
+            <h2 class="font-bold mb-2">Your Shelf</h2>
+            <ul>
+                <li class="mb-4"><a href="{{ route('books.index') }}" class="btn w-full">My Books</a></li>
+                <li class="mb-4"><a href="{{ route('books.add') }}" class="btn w-full">Add a book</a></li>
 
-    <main class="container">
-        {{ $slot }}
-    </main>
+            </ul>
+            @endauth
+        </aside>
+
+        <main class="flex-1 container">
+            {{ $slot }}
+        </main>
+    </div>
 
 </body>
 </html>

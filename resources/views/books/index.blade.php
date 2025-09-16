83d50743d03a61c559708b54dd590ddf6ec69425
Revontulian <pablo.canteria@gmail.com>
2025-09-16 12:24:13 +0200

    Add visual differentiation for private books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
     @if ($user->id == Auth::id())
-    <h2>My Shelf - Owned Books</h2>
+    <h2>My Library</h2>
     @else
     <h2>{{ $user->name }}'s Public Library</h2>
     @endif
@@ -9,7 +9,7 @@
     <ul>
         @foreach ($books as $book)
         <li>
-            <x-card href="{{ route('books.show', $book->id) }}" :highlight="$book['owner_user_id'] == Auth::id()">
+            <x-card href="{{ route('books.show', $book->id) }}" :highlight="$book['owner_user_id'] == Auth::id()" :private="!$book['is_public']">
                 <h3>{{ $book->title }}</h3>
             </x-card>
         </li>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
-@props(['highlight' => false])
+@props(['highlight' => false, 'private' => false])
 
-<div @class(['highlight' => $highlight, 'card'])>
+<div @class(['highlight' => $highlight, 'private' => $private, 'card'])>
     {{ $slot }}
     <a href="{{ $attributes->get('href') }}" class="btn">View Details</a>
 </div>
